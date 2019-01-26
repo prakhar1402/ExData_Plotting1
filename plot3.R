8b7a65898cbd55e5ab55f3eafdaa2b740a57e6c0
prakhar1402 <36913440+prakhar1402@users.noreply.github.com>
2019-01-26 16:04:31 +0530

    Add files via upload
    plot3

---
+++ plot3.R
@@ -0,0 +1,13 @@
+data<-read.table("C:/Users/PRAX/Downloads/Compressed/exdata_data_household_power_consumption/household_power_consumption.txt",header=TRUE,sep=";",dec=".")
+subdata<-data[data$Date %in% c("1/2/2007","2/2/2007"),]
+globalactive<-as.numeric(subdata$Global_active_power)
+datet<-strptime(paste(subdata$Date,subdata$Time,sep=" "),"%d/%m/%Y %H:%M:%S")
+png("plot3.png",width=480,height = 480)
+energy1<-as.numeric(subdata$Sub_metering_1)
+energy2<-as.numeric(subdata$Sub_metering_2)
+energy3<-as.numeric(subdata$Sub_metering_3)
+plot(datet,energy1,ylab="Energy sub metering",xlab="",type="l")
+lines(datet,energy2,type="l",col="red")
+lines(datet,energy3,type="l",col="blue")
+legend("topright",c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),lty = 1,lwd = 2,col=c("black","red","blue"))
+dev.off()
